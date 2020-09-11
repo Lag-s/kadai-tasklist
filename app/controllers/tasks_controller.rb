76602d8cf4579340564dc7c534b2ebd1fc0b21bc
Lag <lagt1307@gmail.com>
2020-09-11 09:33:00 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
 
   def index
     @tasks = current_user.tasks
